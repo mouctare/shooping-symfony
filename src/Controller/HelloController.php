e72fcd7c0d6c2611d8bfe4a62fb722baeba39de1
mouctare <mouctard78@gmail.com>
2021-01-05 15:39:01 +0100

    fisrst commit

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -17,15 +17,9 @@ class HelloController
     /**
     *@Route("/hello/{prenom}", name="hello")
      */
-    public function hello($prenom = "world", LoggerInterface $logger, Calculator $calculator, Environment $twig, Detector $detector){ 
-        dump($detector->detect(101));
-        dump($detector->detect(10));
-
-       // dump($twig);
-        // il ya plusierus façon de se faire livrer un ijection de dependance ici je l'utilise directement dans ma function
-        $logger->info("Mon Message de log");
-        $tva = $calculator->calcul(100);
-        dd($tva);
+    public function hello($prenom = "world"){ 
+       
+        
         return new Response("Bonjour $prenom");
 
     }
